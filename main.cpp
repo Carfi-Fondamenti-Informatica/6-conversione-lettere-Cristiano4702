30c87521821131a7fc4a0462b018d2b8bf298e33
Cristiano4702 <91879010+Cristiano4702@users.noreply.github.com>
2021-12-01 23:38:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,45 @@
 #include <iostream>
 
-int main(){
-  return 0;
+bool verifica(char x){
+
+    if( ((x>='a') && (x<='z')) || ((x>='A') && (x<='Z')) ) {
+
+        return true;
+
+    }return false;
+
+}
+
+char conversione(char x) {
+
+    char ris;
+
+    if (x >= 'A' && x <= 'Z') {
+
+        ris = x + 32;
+
+    } else if (x >= 'a' && x <= 'z') {
+
+        ris = x - 32;
+    }
+}
+int main() {
+
+    char x;
+
+    std::cin >> x;
+
+    bool z =verifica(x);
+
+    if(verifica(x)==true){
+
+        std::cout << conversione(x) <<std::endl;
+
+    }else {
+
+        std::cout << "errore" <<std::endl;
+
+    }
+
+    return 0;
 }
